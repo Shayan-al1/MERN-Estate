3336ca9a149b482d75098c7926756371563264d6
Shayan Ali <shayan.dota88@gmail.com>
2024-01-03 23:20:28 +0500

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
-
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
